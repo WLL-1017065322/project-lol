9f36f3c2ff83274bb90094ad9294776db54c5356
WLL-1017065322 <1017065322@qq.com>
2019-10-09 20:08:54 +0800

    22

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   outputDir: __dirname + "/../server/web",
-  // publicPath: process.env.NODE_ENV === "production" ? "/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/web" : "/"
 };
